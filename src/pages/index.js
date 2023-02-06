55795d487630fabf9732c9474b42e3d2a6cf866c
sariomari <73112567+sariomari@users.noreply.github.com>
2023-02-06 11:10:24 +0200

    Fixed a theme bug.

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,13 +139,13 @@ export default function Home() {
             <div name="contact" className="w-full h-screen flex justify-center items-center">
               <form action="https://getform.io/f/6647e865-b783-4400-ba14-849d9553ebee" method="POST" className="flex flex-col max-w-[600px] w-full">
                 <div className="pb-8">
-                  <p className="text-4xl font-bold inline border-b-4 border-cyan-300 text-white">Let&apos;s get in touch!</p>
+                  <p className="text-4xl font-bold inline border-b-4 border-cyan-300 dark:text-white">Let&apos;s get in touch!</p>
                   <p className="py-3">Either submit this form or send me an email at sari.omari@gmail.com</p>
                 </div>
                 <input className="bg-cyan-50 rounded-md" type="text" placeholder="Name" name="name" />
                 <input className="my-4 bg-cyan-50 rounded-md" type="text" placeholder="Email" name="email" />
                 <textarea className="bg-cyan-50 rounded-md" type="text" placeholder="Message" name="message" rows="10" />
-                <button className="text-white border-2 hover:bg-cyan-300 hover: border-cyan-300 px-4 py-3 my-8 mx-auto flex items-center rounded-2xl">Submit</button>
+                <button className="text-black border-2 hover:bg-cyan-300 hover: border-cyan-300 px-4 py-3 my-8 mx-auto flex items-center rounded-2xl dark:text-white">Submit</button>
 
               </form>
             </div>
